48d1ca93214242f37d1b0febacfe1ef76d7c1ff5
Md Shahanshah Ansari <shahanshah.967@gmail.com>
2023-08-31 19:56:46 +0530

    first commit

---
+++ 01_Basic/03_ConversionOperation.js
@@ -0,0 +1,15 @@
+let score = 'a'
+
+
+console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
